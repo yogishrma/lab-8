6d676e61353abf5078e49f3570889945ba1a09a2
visualstudio <visualstudio@122-PC40>
2019-03-06 11:29:13 -0500

    la 8

--- lab 8/Program.cs
+++ lab 8/Program.cs
@@ -1,15 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace lab_8
+namespace DelegatesAndEvents
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
+            Console.ReadLine();
+        }
+
+    }
+    public delegate void MyDelegate();
+
+    public class DelegateExercises
+    {
+        void Method1()
+        {
+            System.Console.WriteLine("Method1");
+        }
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
         }
     }
+
 }
